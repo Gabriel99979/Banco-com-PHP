9399317b2a1f1fa58e2687ed2044d10364df2ebc
Gabriel Amaral <gabriel99979@gmail.com>
2023-02-07 11:10:48 -0300

    Utilizando método mágico toString

---
+++ enderecos.php
@@ -0,0 +1,13 @@
+<?php
+
+use Alura\Banco\Modelo\Endereco;
+
+require_once 'autoload.php';
+
+$umEndereco = new Endereco('Petropolis', 'Bairro Qualquer', 'Minha Rua','356');
+$outroEndereco = new Endereco('Rio', 'Centro', 'Uma Rua','50');
+
+// echo $umEndereco->recuperaRua() . ", " . $umEndereco->recuperaBairro() . ", " . ou chamar endereco depois de ter impementado toString
+
+echo $umEndereco . PHP_EOL;
+echo $outroEndereco;
